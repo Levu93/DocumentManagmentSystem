a9a9225d96d65c5bdef16f87b450b263202bc044
Levu93 <vulepejovic@hotmail.com>
2017-02-15 19:15:17 +0100

    Spring configuration

---
+++ src/main/java/org/fon/documentmanagementsystem/config/AppConfig.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.fon.documentmanagementsystem.config;
+
+import org.fon.documentmanagementsystem.config.security.SecurityConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ *
+ * @author Vukasin
+ */
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = {"org.fon.documentmanagementsystem.*"})
+@EnableTransactionManagement
+@EnableJpaRepositories("org.fon.documentmanagementsystem.*")
+@Import({SecurityConfig.class})
+public class AppConfig extends WebMvcConfigurerAdapter{
+    
+    @Bean(name = "viewResolver")
+    public InternalResourceViewResolver getViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+    
+}
