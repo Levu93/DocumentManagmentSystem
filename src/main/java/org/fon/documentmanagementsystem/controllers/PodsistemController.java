8ae38fee4e236ae4438468cf067bfff59f0fe15e
NevenaCeh <necaceh@gmail.com>
2017-02-19 17:23:52 +0100

    update i insert subsystems radi, delete mislim da ne treba da pravimo kad zavrsis usere cu da dodam u detalje subsystema sve usere

--- src/main/java/org/fon/documentmanagementsystem/controllers/PodsistemController.java
+++ src/main/java/org/fon/documentmanagementsystem/controllers/PodsistemController.java
@@ -6,13 +6,15 @@
 package org.fon.documentmanagementsystem.controllers;
 
 import java.util.List;
+import java.util.Optional;
+import jdk.nashorn.internal.runtime.options.Option;
 import org.fon.documentmanagementsystem.domain.Podsistem;
 import org.fon.documentmanagementsystem.services.PodsistemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -22,12 +24,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/subsystem/")
 public class PodsistemController {
-    
+
     @Autowired
     private PodsistemService podsistemService;
-    
+
     @RequestMapping(path = "/overview", method = RequestMethod.GET)
-    public ModelAndView home(){
+    public ModelAndView home() {
         ModelAndView mv = new ModelAndView("subsystem_overview");
         System.out.println("U kontroleru sam za prikaz svih");
         List<Podsistem> sviPodsistemi;
@@ -35,31 +37,56 @@ public class PodsistemController {
         mv.addObject("subsystems", sviPodsistemi);
         return mv;
     }
-    
+
     @RequestMapping(path = "/add_new_subsystem", method = RequestMethod.GET)
-    public ModelAndView addSubsystem(){
+    public ModelAndView addSubsystem() {
         ModelAndView mv = new ModelAndView("subsystem_add");
         System.out.println("U kontroleru sam");
         return mv;
     }
-    
-        @RequestMapping(path = "/add_new_subsystem", method = RequestMethod.POST)
-    public ModelAndView addNewSubsystem(String subsystemname, String subsystemsign, String subsystemdescription){
-        
+
+    @RequestMapping(path = "/add_new_subsystem", method = RequestMethod.POST)
+    public ModelAndView addNewSubsystem(String subsystemname, String subsystemsign, String subsystemdescription) {
         Podsistem ps = new Podsistem();
-        int id1 = podsistemService.vratiId()+1;
+        int id1 = podsistemService.vratiId() + 1;
         long x = id1;
         ps.setId(x);
         ps.setNaziv(subsystemname);
         ps.setOznaka(subsystemsign);
         ps.setOpis(subsystemdescription);
-        
-        podsistemService.sacuvajPodsistem(ps);        
-        
+        podsistemService.sacuvajPodsistem(ps);
         ModelAndView mv = new ModelAndView("subsystem_overview");
         List<Podsistem> sviPodsistemi;
         sviPodsistemi = podsistemService.findAll();
         mv.addObject("subsystems", sviPodsistemi);
+        return mv;
+    }
+    
+    @RequestMapping(path = "/add_new_subsystem/{id}", method = RequestMethod.POST)
+    public ModelAndView update(@PathVariable("id") long id, String subsystemname, String subsystemdescription) {
+        Podsistem ps = podsistemService.findOne(id);
+        
+        ps.setNaziv(subsystemname);
+        ps.setOpis(subsystemdescription);
+        podsistemService.sacuvajPodsistem(ps);
+        ModelAndView mv = new ModelAndView("subsystem_overview");
+        List<Podsistem> sviPodsistemi;
+        sviPodsistemi = podsistemService.findAll();
+        mv.addObject("subsystems", sviPodsistemi);
+        return mv;
+    }
+
+    @RequestMapping(path = "/details/{id}", method = RequestMethod.GET)
+    public ModelAndView showSubsystem(@PathVariable("id") long id
+    ) {
+        System.out.println("kontroler za prikaz sa id " + id);
+        Podsistem podsistem = podsistemService.findOne(id);
+        // if (podsistem == null) throw new CustomException("There is no company with id " + id, "404");
+        // List<User> usersOfCompany = userService.findUsersOfCompany(company);
+        // System.out.println("@@@@@@@@@@@ " + usersOfCompany);
+        ModelAndView mv = new ModelAndView("subsystem_add");
+        mv.addObject("subsystem", podsistem);
+        // mv.addObject("users", usersOfCompany);
         return mv;
     }
 }
